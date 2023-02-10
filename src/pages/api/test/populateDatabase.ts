6155c0f84e896acf85866d4dc2334722c8fc2120
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2023-02-10 14:25:38 -0800

    wip: Complete functions that query, update/create records, and return them in a response

--- src/pages/api/test/populateDatabase.ts
+++ src/pages/api/test/populateDatabase.ts
@@ -1,7 +1,7 @@
 import Newsapi, { ApiNewsCategory } from '../../../lib/newsapi';
 import { PrismaClient } from '@prisma/client';
+import { v4 } from 'uuid';
 
-const newsClient = new Newsapi(process.env.NEWS_API_KEY);
 const defaultCategories: ApiNewsCategory[] = [
   'business',
   // 'entertainment',
@@ -10,19 +10,17 @@ const defaultCategories: ApiNewsCategory[] = [
   // 'politics',
 ];
 
-async function getTopNewsByCategory(category: ApiNewsCategory) {
-  const topHeadlines = await newsClient.getTopHeadlines({
-    country: 'us',
-    category,
-    pageSize: 20,
-    page: 1,
-  });
+export default async function handler(req, res) {
+  const articles = await getDefaultNews();
 
-  return topHeadlines.articles;
+  res.status(200).json(articles);
 }
 
 async function getDefaultNews() {
-  // P1: Query all categories that are defined as default news categories.
+  /**
+   * P1: Query all categories that are defined as default news categories.
+   * TODO: If a category is not found, create it.
+   */
   const prisma = new PrismaClient();
   const categories = await prisma.category.findMany({
     where: {
@@ -32,16 +30,20 @@ async function getDefaultNews() {
     },
   });
 
-  // P:2 Traverse each category, and check the lastUpdated field.
+  /**
+   * P:2 Traverse each category, and check the lastUpdated field.
+   */
   const promises = categories.map(async (category) => {
     const lastUpdated = new Date(category.lastUpdated);
     const now = new Date();
     const diff = now.getTime() - lastUpdated.getTime();
     const diffHours = Math.floor(diff / 1000 / 60 / 60);
 
-    // P:2.1 If the lastUpdated field is older than 1 day,
-    // then query NewsAPI for new articles from that category type.
-    if (diffHours > 1) {
+    /**
+     * P:2.1 If the lastUpdated field is older than 1 hour,
+     * then query NewsAPI for new articles from that category type.
+     */
+    if (diffHours > 0.5) {
       const articles = await getTopNewsByCategory(
         category.type as ApiNewsCategory
       );
@@ -49,8 +51,9 @@ async function getDefaultNews() {
        * P:2.1.1 Add all the articles returned into the Articles table.
        * The category field must be set to the category that was queried.
        */
-      const newArticles = await prisma.article.createMany({
+      await prisma.article.createMany({
         data: articles.map((article) => ({
+          id: article.id,
           author: article.author,
           title: article.title,
           description: article.description,
@@ -67,18 +70,18 @@ async function getDefaultNews() {
           },
         })),
       });
-      const articleIds = newArticles.data.map((article) => article.id);
 
-      /** P: 2.1.2
-       *  Update that category to have reference to each article just queried.
+      /**
+       * P: 2.1.2 Update that category to have reference
+       * to each article just queried.
        */
-      await prisma.category.update({
+      const updatedCategory = await prisma.category.update({
         where: {
           id: category.id,
         },
         data: {
           articles: {
-            connect: newArticles.map((article) => ({
+            connect: articles.map((article) => ({
               id: article.id,
             })),
           },
@@ -86,49 +89,43 @@ async function getDefaultNews() {
         },
       });
 
-      // const updatedCategory = await prisma.category.update({
-      //   where: {
-      //     id: category.id,
-      //   },
-      //   data: {
-      //     articles: {
-      //       create: articles.map((article) => ({
-      //         author: article.author,
-      //         title: article.title,
-      //         description: article.description,
-      //         content: article.content,
-      //         url: article.url,
-      //         urlToImage: article.urlToImage,
-      //         publishedAt: article.publishedAt,
-      //         sourceId: article.source.name,
-      //         sourceName: article.source.name,
-      //         categories: {
-      //           connect: {
-      //             id: category.id,
-      //           },
-      //         },
-      //       })),
-      //     },
-      //     lastUpdated: now,
-      //   },
-      // });
-      // return updatedCategory;
+      return updatedCategory;
     } else {
       return category;
     }
   });
 
-  // const nestedArticles = await Promise.all(promises);
-  // const newsArticles = nestedArticles.reduce(
-  //   (acc, articles) => acc.concat(articles),
-  //   []
-  // );
+  /**
+   * P:3 Return a complete collection of all
+   * articles under these default categories, now that it's all up to date.
+   */
+  const updatedCategories = await Promise.all(promises);
+  const allArticles = await prisma.article.findMany({
+    where: {
+      categories: {
+        some: {
+          id: {
+            in: updatedCategories.map((category) => category.id),
+          },
+        },
+      },
+    },
+  });
 
-  return categories;
+  return allArticles;
 }
 
-export default async function handler(req, res) {
-  const categories = await getDefaultNews();
+async function getTopNewsByCategory(category: ApiNewsCategory) {
+  const newsClient = new Newsapi(process.env.NEWS_API_KEY);
+  const topHeadlines = await newsClient.getTopHeadlines({
+    country: 'us',
+    category,
+    pageSize: 20,
+    page: 1,
+  });
 
-  res.status(200).json(categories);
+  return topHeadlines.articles.map((article) => ({
+    id: v4() as string,
+    ...article,
+  }));
 }
