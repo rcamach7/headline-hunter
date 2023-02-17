a189a44d03c5d3a72634caa39a3cf3a911957465
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2023-02-16 18:24:01 -0800

    feat: Add next auth context to frontend and build test page

--- src/pages/test/auth.tsx
+++ src/pages/test/auth.tsx
@@ -1,7 +1,31 @@
+import { signIn, signOut } from 'next-auth/react';
+import { useSession } from 'next-auth/react';
+
 export default function Home() {
+  const userSession = useSession();
+
   return (
-    <div>
-      <h1>Test Page</h1>
+    <div
+      style={{
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      {userSession.status === 'loading' ? (
+        <div>Loading...</div>
+      ) : userSession.status === 'unauthenticated' ? (
+        <button onClick={() => signIn()}>Sign in</button>
+      ) : (
+        <div>
+          <div>Authenticated</div>
+          <div>{userSession.data.user.email}</div>
+          <button onClick={() => signOut()}>Sign out</button>
+        </div>
+      )}
     </div>
   );
 }
