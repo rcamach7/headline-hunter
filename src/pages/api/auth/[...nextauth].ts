4f2488a1a27caa3eec876236c7499affd0183aa4
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2023-02-16 17:17:50 -0800

    wip: Add prisma as adapter for next auth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import prisma from '@/lib/prisma';
 
 export const authOptions = {
   providers: [
@@ -8,6 +10,7 @@ export const authOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
+  adapter: PrismaAdapter(prisma),
 };
 
 export default NextAuth(authOptions);
