3225513b049fb93dfc914708860054f48b43d5b3
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2023-02-20 14:43:21 -0800

    refactor: Pull styled components for app bar into their own file

--- src/components/organisms/AppBar/AppBar.tsx
+++ src/components/organisms/AppBar/AppBar.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
-import { styled, alpha } from '@mui/material/styles';
+import { Search, SearchIconWrapper, StyledInputBase } from './AppBar.styled';
 import {
-  InputBase,
   AppBar,
   Box,
   Toolbar,
@@ -17,48 +16,6 @@ import Image from 'next/image';
 
 const pages = ['Products', 'Pricing', 'Blog'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
-
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
-    marginLeft: theme.spacing(1),
-    width: 'auto',
-  },
-}));
-
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  '& .MuiInputBase-input': {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      width: '12ch',
-      '&:focus': {
-        width: '20ch',
-      },
-    },
-  },
-}));
 
 export default function SearchAppBar() {
   const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
