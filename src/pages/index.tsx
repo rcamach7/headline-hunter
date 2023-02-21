73a045934e5695fa3720e52769d5d914607cd796
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2023-02-20 17:52:23 -0800

    chore: Add title to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,11 @@
+import Head from 'next/head';
+
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline"></h1>;
+  return (
+    <>
+      <Head>
+        <title>Headline Hunter</title>
+      </Head>
+    </>
+  );
 }
