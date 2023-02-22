72066fec4fdee4092b60e5f45b3c6c40c620c23e
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2023-02-22 13:43:13 -0800

    feat: Create additional blank pages

---
+++ src/pages/account.tsx
@@ -0,0 +1,11 @@
+import Head from 'next/head';
+
+export default function Account() {
+  return (
+    <>
+      <Head>
+        <title>HH: My Account</title>
+      </Head>
+    </>
+  );
+}
---
+++ src/pages/news.tsx
@@ -0,0 +1,11 @@
+import Head from 'next/head';
+
+export default function News() {
+  return (
+    <>
+      <Head>
+        <title>HH: News</title>
+      </Head>
+    </>
+  );
+}
