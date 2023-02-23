f42c4070bee8e9bb89f8999868f0078094205ec3
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2023-02-22 19:50:15 -0800

    wip: Add default categories and favorited if applicable to menu

--- src/components/organisms/AppBar/CategoryMenuItems.tsx
+++ src/components/organisms/AppBar/CategoryMenuItems.tsx
@@ -3,7 +3,7 @@ import { Typography, MenuItem, Divider } from '@mui/material';
 import { Preferences } from '@/context/UserContext.types';
 
 interface Props {
-  preferences: Preferences;
+  preferences: Preferences | null;
   handleCloseNavMenu: () => void;
 }
 
@@ -21,15 +21,24 @@ export default function CategoryMenuItems({
 }: Props) {
   return (
     <>
-      {/* <MenuItem
-        disabled={true}
-        key={page}
-        onClick={handleCloseNavMenu}
-        sx={{ background: 'background.default' }}
-      >
-        <Typography textAlign="center">{page}</Typography>
+      {preferences.savedCategories && (
+        <MenuItem disabled={true} sx={{ py: 0, height: 10 }}>
+          <Typography textAlign="center" fontSize={12} sx={{ p: 0 }}>
+            Favorite Categories
+          </Typography>
+        </MenuItem>
+      )}
+
+      <MenuItem disabled={true} sx={{ py: 0, height: 10 }}>
+        <Typography textAlign="center" fontSize={12} sx={{ p: 0 }}>
+          Default Categories
+        </Typography>
       </MenuItem>
-      <Divider /> */}
+      {defaultCategories.map((category) => (
+        <MenuItem key={category} onClick={handleCloseNavMenu} sx={{ py: 0 }}>
+          {category}
+        </MenuItem>
+      ))}
     </>
   );
 }
