fb1512f5ec2acbca1fd7fe8f98eff51492441f98
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2023-03-08 18:49:35 -0800

    wip: Add is focused property for textfield dynamic grown on focus

--- src/components/organisms/AppBar/Search.tsx
+++ src/components/organisms/AppBar/Search.tsx
@@ -1,15 +1,29 @@
+import { useState } from 'react';
 import { default_categories } from '@/lib/categories';
 import { TextField, Autocomplete, Box } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 
 export default function Search() {
+  const [isFocused, setIsFocused] = useState(false);
+
+  const handleFocus = () => {
+    setIsFocused(true);
+  };
+
+  const handleBlur = () => {
+    setIsFocused(false);
+  };
+
   return (
     <Box
       sx={{
+        maxWidth: isFocused ? 300 : 200,
+        flexGrow: 1,
         display: 'flex',
-        justifyContent: 'center',
         alignItems: 'center',
-        outline: 'auto',
+        backgroundColor: '#5D5D5D',
+        borderRadius: 1,
+        p: 0.5,
       }}
     >
       <SearchIcon />
@@ -17,8 +31,14 @@ export default function Search() {
         id="free-solo-demo"
         options={default_categories.map((option) => option.title)}
         renderInput={(params) => (
-          <TextField {...params} label="Search..." variant="standard" />
+          <TextField
+            {...params}
+            variant="standard"
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+          />
         )}
+        sx={{ flexGrow: 1 }}
       />
     </Box>
   );
