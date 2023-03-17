df69fa5c9ebd09d4f0be92a8e1d337833cf1c69d
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2023-03-16 19:27:50 -0700

    wip: Create basic dynamic button based on favorited status

--- src/components/atoms/FavoriteCategoryButton.tsx
+++ src/components/atoms/FavoriteCategoryButton.tsx
@@ -1,10 +1,54 @@
 import { Button } from '@mui/material';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Preferences } from '@/lib/types';
 
+interface Props {
+  category: string;
+  preferences: Preferences;
+}
+
 export default function FavoriteCategoryButton({
-  category: string,
-  preferences: Preferences,
-}) {
-  return <Button>Favorite Category</Button>;
+  category,
+  preferences,
+}: Props) {
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  useEffect(() => {
+    if (preferences) {
+      const { savedCategories } = preferences;
+      if (savedCategories) {
+        const isFavorite = savedCategories.some(
+          (savedCategory) => savedCategory.type === category
+        );
+        setIsFavorite(isFavorite);
+      }
+    }
+  }, [preferences]);
+
+  if (!preferences) return null;
+  if (isFavorite) {
+    return (
+      <Button
+        variant="contained"
+        color="secondary"
+        onClick={() => {
+          setIsFavorite(false);
+        }}
+      >
+        Remove from Favorites
+      </Button>
+    );
+  } else {
+    return (
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => {
+          setIsFavorite(true);
+        }}
+      >
+        Add to Favorites
+      </Button>
+    );
+  }
 }
