1dfdc63bae4b4eaff37357b47931157b4577cf1f
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2023-03-20 20:27:42 -0700

    wip: Create functionality to toggle avorited category

--- src/pages/api/user/favoriteCategories/[cid].ts
+++ src/pages/api/user/favoriteCategories/[cid].ts
@@ -1,19 +1,40 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { authOptions } from '@/auth/[...nextauth]';
+import { getServerSession } from 'next-auth/next';
+import {
+  getUserByEmail,
+  toggleCategoryFavorited,
+} from '@/services/userService';
 import { getCategoryById } from '@/services/categoryService';
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  const session = await getServerSession(req, res, authOptions);
+  if (!session) res.status(401).json({ message: 'Unauthorized' });
+
   const { cid } = req.query;
   if (!cid || Array.isArray(cid))
-    return res.status(400).json({ message: 'Missing category id field' });
+    return res.status(400).json({ message: 'Missing categoryId field' });
 
   switch (req.method) {
     case 'POST':
-      const category = await getCategoryById(cid as string);
-      if (!category)
-        return res.status(404).json({ message: 'Category not found' });
+      const user = await getUserByEmail(session.user.email);
+      const category = await getCategoryById(cid);
+      if (!user)
+        return res
+          .status(404)
+          .json({ message: 'Error retrieving user or category' });
+
+      try {
+        await toggleCategoryFavorited(user, category);
+        return res.status(200).json({ message: 'Success' });
+      } catch (error) {
+        return res
+          .status(500)
+          .json({ message: 'Error toggling favorited status' });
+      }
 
     default:
       res.setHeader('Allow', ['POST']);
---
+++ src/services/userService.ts
@@ -0,0 +1,47 @@
+import prisma from '@/lib/prisma';
+import { Article, Category, User } from '@prisma/client';
+import { v4 } from 'uuid';
+
+export async function getUserByEmail(email: string) {
+  return await prisma.user.findUnique({
+    where: { email: email },
+    include: {
+      savedArticles: true,
+      savedCategories: true,
+      dislikedCategories: true,
+    },
+  });
+}
+
+export async function toggleCategoryFavorited(
+  user: User & {
+    savedCategories: Category[];
+    savedArticles: Article[];
+    dislikedCategories: Category[];
+  },
+  category: Category
+) {
+  const isFavorited = user.savedCategories.some(
+    (savedCategory) => savedCategory.id === category.id
+  );
+
+  if (isFavorited) {
+    await prisma.user.update({
+      where: { id: user.id },
+      data: {
+        savedCategories: {
+          disconnect: { id: category.id },
+        },
+      },
+    });
+  } else {
+    await prisma.user.update({
+      where: { id: user.id },
+      data: {
+        savedCategories: {
+          connect: { id: category.id },
+        },
+      },
+    });
+  }
+}
