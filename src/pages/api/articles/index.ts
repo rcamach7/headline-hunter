804d9c9fcc553b790a1ea77d596905402cbf711c
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2023-03-23 20:22:28 -0700

    feat: Ensure at least 5 categories are queried if user hasn't favorited that many categories

--- src/pages/api/articles/index.ts
+++ src/pages/api/articles/index.ts
@@ -3,7 +3,7 @@ import { getServerSession } from 'next-auth/next';
 
 import { authOptions } from '@/auth/[...nextauth]';
 import { popularCategories } from '@/lib/data';
-import { CategoryNews, getNewsByCategories } from '@/services/articleService';
+import { getNewsByCategories } from '@/services/articleService';
 import { getCategoriesByIds } from '@/services/categoryService';
 
 export default async function handler(
@@ -15,22 +15,24 @@ export default async function handler(
       case 'GET':
         const session = await getServerSession(req, res, authOptions);
 
-        if (!session) {
-          const categories = await getCategoriesByIds(
-            popularCategories.map((category) => category.id)
-          );
-          const newsCategories = await getNewsByCategories(categories);
-          return res.status(200).json({ newsCategories });
-        } else {
+        if (session) {
           const user = await prisma.user.findUnique({
             where: { email: session.user.email },
             include: {
               savedCategories: true,
             },
           });
-          const newsCategories: CategoryNews[] = await getNewsByCategories(
+
+          const selectedCategories = getSelectedCategories(
             user.savedCategories
           );
+          const newsCategories = await getNewsByCategories(selectedCategories);
+          return res.status(200).json({ newsCategories });
+        } else {
+          const categories = await getCategoriesByIds(
+            popularCategories.map((category) => category.id)
+          );
+          const newsCategories = await getNewsByCategories(categories);
           return res.status(200).json({ newsCategories });
         }
 
@@ -42,4 +44,24 @@ export default async function handler(
     console.error(error);
     res.status(500).json({ message: 'An unexpected error occurred' });
   }
+}
+
+function getSelectedCategories(savedCategories) {
+  let selectedCategories = [...savedCategories];
+
+  if (selectedCategories.length < 5) {
+    const additionalCategoriesNeeded = 5 - selectedCategories.length;
+    const additionalCategories = popularCategories
+      .filter(
+        (category) =>
+          !selectedCategories.find(
+            (savedCategory) => savedCategory.id === category.id
+          )
+      )
+      .slice(0, additionalCategoriesNeeded);
+
+    selectedCategories = [...selectedCategories, ...additionalCategories];
+  }
+
+  return selectedCategories;
 }
