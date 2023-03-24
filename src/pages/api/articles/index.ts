4b46808a1564b30b2a62756ac846d8ce8f7bd27d
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2023-03-23 20:11:44 -0700

    feat: Implement dynamic endpoint for popular categories

--- src/pages/api/articles/index.ts
+++ src/pages/api/articles/index.ts
@@ -4,31 +4,42 @@ import { getServerSession } from 'next-auth/next';
 import { authOptions } from '@/auth/[...nextauth]';
 import { popularCategories } from '@/lib/data';
 import { CategoryNews, getNewsByCategories } from '@/services/articleService';
-import {
-  getCategoryById,
-  getCategoriesByIds,
-} from '@/services/categoryService';
+import { getCategoriesByIds } from '@/services/categoryService';
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  switch (req.method) {
-    case 'GET':
-      const session = await getServerSession(req, res, authOptions);
+  try {
+    switch (req.method) {
+      case 'GET':
+        const session = await getServerSession(req, res, authOptions);
 
-      if (!session) {
-        const categories = await getCategoriesByIds(
-          popularCategories.map((c) => c.id)
-        );
-        const newsCategories = await getNewsByCategories(categories);
-        return res.status(200).json({ newsCategories });
-      } else {
-      }
+        if (!session) {
+          const categories = await getCategoriesByIds(
+            popularCategories.map((category) => category.id)
+          );
+          const newsCategories = await getNewsByCategories(categories);
+          return res.status(200).json({ newsCategories });
+        } else {
+          const user = await prisma.user.findUnique({
+            where: { email: session.user.email },
+            include: {
+              savedCategories: true,
+            },
+          });
+          const newsCategories: CategoryNews[] = await getNewsByCategories(
+            user.savedCategories
+          );
+          return res.status(200).json({ newsCategories });
+        }
 
-      break;
-    default:
-      res.setHeader('Allow', ['GET']);
-      res.status(405).end(`Method ${req.method} Not Allowed`);
+      default:
+        res.setHeader('Allow', ['GET']);
+        res.status(405).end(`Method ${req.method} Not Allowed`);
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'An unexpected error occurred' });
   }
 }
