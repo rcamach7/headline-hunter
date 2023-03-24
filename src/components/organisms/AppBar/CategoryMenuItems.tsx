4eca4ef8a97c04b4e89b201f937bbf210f93a319
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2023-03-23 18:34:01 -0700

    refactor: Export default category into data file and rename variable to popularCategories

--- src/components/organisms/AppBar/CategoryMenuItems.tsx
+++ src/components/organisms/AppBar/CategoryMenuItems.tsx
@@ -2,34 +2,12 @@ import * as React from 'react';
 import { Typography, MenuItem, Divider } from '@mui/material';
 import { User } from '@/lib/types';
 import Link from 'next/link';
+import { popularCategories } from '@/lib/data';
 
 interface Props {
   user: User;
   handleCloseNavMenu: () => void;
 }
-
-const defaultCategories = [
-  {
-    id: '77d0b6a2-4685-4a5e-9d60-87e05fa5356e',
-    type: 'Business',
-  },
-  {
-    id: '2c6a96f2-25cc-4afd-b78a-eeeb54e58847',
-    type: 'Entertainment',
-  },
-  {
-    id: '928a68a2-7387-4305-b8a8-870ba0929df6',
-    type: 'Sports',
-  },
-  {
-    id: '44d25b34-6778-41cf-a09b-75f7fba28fec',
-    type: 'Technology',
-  },
-  {
-    id: 'b1dfe2c4-14e0-4a14-8ff4-ed8a7df49601',
-    type: 'Politics',
-  },
-];
 
 export default function CategoryMenuItems({ handleCloseNavMenu, user }: Props) {
   return (
@@ -47,10 +25,10 @@ export default function CategoryMenuItems({ handleCloseNavMenu, user }: Props) {
 
       <MenuItem disabled={true} sx={{ py: 0 }} dense>
         <Typography textAlign="center" fontSize={12} sx={{ p: 0 }}>
-          Default
+          Popular
         </Typography>
       </MenuItem>
-      {defaultCategories.map((category) => (
+      {popularCategories.map((category) => (
         <MenuItem key={category.id} onClick={handleCloseNavMenu}>
           <Link href={`/news/category/${category.id}`} passHref>
             <Typography
---
+++ src/lib/data.ts
@@ -0,0 +1,22 @@
+export const popularCategories = [
+  {
+    id: '77d0b6a2-4685-4a5e-9d60-87e05fa5356e',
+    type: 'business',
+  },
+  {
+    id: '2c6a96f2-25cc-4afd-b78a-eeeb54e58847',
+    type: 'entertainment',
+  },
+  {
+    id: '928a68a2-7387-4305-b8a8-870ba0929df6',
+    type: 'sports',
+  },
+  {
+    id: '44d25b34-6778-41cf-a09b-75f7fba28fec',
+    type: 'technology',
+  },
+  {
+    id: 'b1dfe2c4-14e0-4a14-8ff4-ed8a7df49601',
+    type: 'politics',
+  },
+];
