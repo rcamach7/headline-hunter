b61a55fc7e1df5810ab4440aba64d0a71aaf812f
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2023-03-27 22:06:26 -0700

    wip: Add boilerplate MUI carD

--- src/components/molecules/CategoryCard/CategoryCard.tsx
+++ src/components/molecules/CategoryCard/CategoryCard.tsx
@@ -1,3 +1,12 @@
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+} from '@mui/material';
+
 import { CategoryArticles } from '@/lib/types';
 
 interface Props {
@@ -5,5 +14,26 @@ interface Props {
 }
 
 export default function CategoryCard({ categoryArticle }: Props) {
-  return <></>;
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image="/static/images/cards/contemplative-reptile.jpg"
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  );
 }
