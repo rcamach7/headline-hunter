00f3959d42a8c63e3913832d1bbfa9e11476f4cc
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2023-03-28 16:27:08 -0700

    refactor: Pull NewsLine into its own component file

--- src/components/molecules/CategoryCard/CategoryCard.tsx
+++ src/components/molecules/CategoryCard/CategoryCard.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image';
 import { formatDistance } from 'date-fns';
 
 import CategoryTitle from './CategoryTitle';
+import NewsLine from './NewsLine';
 import { CategoryArticles, Article } from '@/lib/types';
 import { removeNewsSource } from '@/lib/helpers';
 
@@ -59,33 +60,6 @@ export default function CategoryCard({ categoryArticle }: Props) {
           <Button onClick={handleShowMore}>Show More</Button>
         </CardContent>
       </Card>
-    </Box>
-  );
-}
-
-interface NLProps {
-  article: Article;
-}
-
-function NewsLine({ article }: NLProps) {
-  return (
-    <Box sx={{ display: 'flex' }}>
-      <Textfit mode="multi" max={40} min={1} style={{ width: '100%' }}>
-        <Typography variant="body2" color="text.primary" gutterBottom>
-          {removeNewsSource(article.title)}
-        </Typography>
-        <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-          <Typography variant="body2" color="text.secondary">
-            {removeNewsSource(article.sourceName)}
-          </Typography>
-
-          <Typography variant="body2" color="text.secondary" gutterBottom>
-            {formatDistance(new Date(article.publishedAt), new Date(), {
-              addSuffix: true,
-            })}
-          </Typography>
-        </Box>
-      </Textfit>
     </Box>
   );
 }
---
+++ src/components/molecules/CategoryCard/NewsLine.tsx
@@ -0,0 +1,33 @@
+import { Box, Typography } from '@mui/material';
+import { Textfit } from 'react-textfit';
+import { formatDistance } from 'date-fns';
+
+import { removeNewsSource } from '@/lib/helpers';
+import { Article } from '@/lib/types';
+
+interface Props {
+  article: Article;
+}
+
+export default function NewsLine({ article }: Props) {
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <Textfit mode="multi" max={40} min={1} style={{ width: '100%' }}>
+        <Typography variant="body2" color="text.primary" gutterBottom>
+          {removeNewsSource(article.title)}
+        </Typography>
+        <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+          <Typography variant="body2" color="text.secondary">
+            {removeNewsSource(article.sourceName)}
+          </Typography>
+
+          <Typography variant="body2" color="text.secondary" gutterBottom>
+            {formatDistance(new Date(article.publishedAt), new Date(), {
+              addSuffix: true,
+            })}
+          </Typography>
+        </Box>
+      </Textfit>
+    </Box>
+  );
+}
