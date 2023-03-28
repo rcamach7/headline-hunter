0419bd8aab9382968b0909cebe354fa8c99dba9f
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2023-03-28 15:53:33 -0700

    feat: Create title for Category card

--- src/components/molecules/CategoryCard/CategoryCard.tsx
+++ src/components/molecules/CategoryCard/CategoryCard.tsx
@@ -1,4 +1,5 @@
-import { Card, CardContent, Box, Typography } from '@mui/material';
+import { Card, CardContent, Box, Typography, Button } from '@mui/material';
+import { ArrowCircleRight as ArrowCircleRightIcon } from '@mui/icons-material/';
 import { Textfit } from 'react-textfit';
 import Image from 'next/image';
 
@@ -14,30 +15,47 @@ export default function CategoryCard({ categoryArticle }: Props) {
   const primaryArticle = articles[1];
 
   return (
-    <Card sx={{ width: 345 }}>
-      <Box sx={{ position: 'relative', height: 175, width: '100%' }}>
-        <Image
-          src={`/api/image-proxy?url=${encodeURIComponent(
-            primaryArticle.urlToImage
-          )}`}
-          alt={primaryArticle.title}
-          layout="fill"
-          objectFit="cover"
-          objectPosition="top"
-        />
+    <Box>
+      <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+        <Typography
+          gutterBottom
+          variant="h6"
+          component="div"
+          sx={{ fontWeight: 'bold', pl: 1 }}
+        >
+          {categoryArticle.type.toUpperCase()}
+        </Typography>
+
+        <Button variant="text" startIcon={<ArrowCircleRightIcon />}>
+          View More
+        </Button>
       </Box>
-      <CardContent>
-        <Textfit mode="multi" max={40} min={1} style={{ width: '100%' }}>
-          <Typography
-            gutterBottom
-            variant="body1"
-            component="div"
-            sx={{ fontWeight: 'bold' }}
-          >
-            {removeNewsSource(primaryArticle.title)}
-          </Typography>
-        </Textfit>
-      </CardContent>
-    </Card>
+
+      <Card sx={{ width: 345 }}>
+        <Box sx={{ position: 'relative', height: 175, width: '100%' }}>
+          <Image
+            src={`/api/image-proxy?url=${encodeURIComponent(
+              primaryArticle.urlToImage
+            )}`}
+            alt={primaryArticle.title}
+            layout="fill"
+            objectFit="cover"
+            objectPosition="top"
+          />
+        </Box>
+        <CardContent>
+          <Textfit mode="multi" max={40} min={1} style={{ width: '100%' }}>
+            <Typography
+              gutterBottom
+              variant="body1"
+              component="div"
+              sx={{ fontWeight: 'bold' }}
+            >
+              {removeNewsSource(primaryArticle.title)}
+            </Typography>
+          </Textfit>
+        </CardContent>
+      </Card>
+    </Box>
   );
 }
