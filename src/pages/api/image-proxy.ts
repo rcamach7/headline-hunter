3740b640489b5b1a88e8c2c169ce99d9e1d01c12
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2023-03-27 23:04:15 -0700

    ci: Add proxy for images so NextJS's Image accepts external image source

---
+++ src/pages/api/image-proxy.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const imageUrl = req.query.url;
+
+  if (!imageUrl || typeof imageUrl !== 'string') {
+    return res.status(400).json({ error: 'Invalid or missing image URL' });
+  }
+
+  try {
+    const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+
+    res.setHeader('Content-Type', response.headers['content-type']);
+    res.setHeader('Cache-Control', 'public, max-age=86400');
+    res.status(200).send(Buffer.from(response.data));
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch image' });
+  }
+}
