c00bd31dd35b18d4840e8ca54b5fa7e73129d0ed
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2023-03-30 12:59:56 -0700

    wip: Set up dynamic action for favorited icon button

--- src/components/molecules/ArticleActionButtons/FavoriteArticle.tsx
+++ src/components/molecules/ArticleActionButtons/FavoriteArticle.tsx
@@ -1,20 +1,60 @@
-import { Box, IconButton, Button } from '@mui/material';
+import { IconButton, CircularProgress } from '@mui/material';
 import {
-  BookmarkBorder as BookmarkBorderIcon,
-  SmartToy as SmartToyIcon,
   FavoriteBorder as FavoriteBorderIcon,
   Favorite as FavoriteFullIcon,
-  ThumbDownOffAlt as ThumbDownOffAltIcon,
 } from '@mui/icons-material';
+import { useState, useEffect } from 'react';
+
+import { useUserContext } from '@/context/UserContext';
 
 interface Props {
   articleId: string;
 }
 
 export default function FavoriteArticle({ articleId }: Props) {
-  return (
-    <IconButton color="secondary" sx={{ padding: 0, ml: 0.5 }}>
-      <FavoriteBorderIcon sx={{ fontSize: 16 }} />
-    </IconButton>
-  );
+  const [isFavorite, setIsFavorite] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const { user, refreshUser } = useUserContext();
+
+  // async function toggleFavorite() {
+  //   try {
+  //     setIsLoading(true);
+  //     await axios.post('/api/user/favoriteCategories/' + categoryId);
+  //     refreshUser();
+  //     setIsFavorite((prev) => !prev);
+  //     setIsLoading(false);
+  //   } catch (error) {
+  //     setIsLoading(false);
+  //     console.error('Error toggling favorite category:', error);
+  //   }
+  // }
+
+  useEffect(() => {
+    if (user) {
+      if (user.savedArticles) {
+        const isFavorited = user.savedArticles.some(
+          (savedArticle) => savedArticle.id === articleId
+        );
+        setIsFavorite(isFavorited);
+      }
+    }
+  }, [user]);
+
+  if (isLoading) {
+    return (
+      <IconButton aria-label="loading" disabled>
+        <CircularProgress size={24} />
+      </IconButton>
+    );
+  } else {
+    return (
+      <IconButton color="secondary" sx={{ padding: 0, ml: 0.5 }}>
+        {isFavorite ? (
+          <FavoriteFullIcon sx={{ fontSize: 16 }} />
+        ) : (
+          <FavoriteBorderIcon sx={{ fontSize: 16 }} />
+        )}
+      </IconButton>
+    );
+  }
 }
