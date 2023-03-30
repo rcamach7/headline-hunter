9b8c300a7322408d8bc5f5d248f5104d2a184274
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2023-03-30 11:59:19 -0700

    fix: Add return statement to end api call

--- src/pages/api/image-proxy.ts
+++ src/pages/api/image-proxy.ts
@@ -16,8 +16,8 @@ export default async function handler(
 
     res.setHeader('Content-Type', response.headers['content-type']);
     res.setHeader('Cache-Control', 'public, max-age=86400');
-    res.status(200).send(Buffer.from(response.data));
+    return res.status(200).send(Buffer.from(response.data));
   } catch (error) {
-    res.status(500).json({ error: 'Failed to fetch image' });
+    return res.status(500).json({ error: 'Failed to fetch image' });
   }
 }
