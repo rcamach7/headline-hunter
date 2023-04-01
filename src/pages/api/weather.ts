48b670a802302fd3286c3998eefe17fd16200c0d
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2023-03-31 18:28:24 -0700

    wip: Add helper function to process and simplify response from weather api

--- src/pages/api/weather.ts
+++ src/pages/api/weather.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import axios from 'axios';
 
-import {} from '@/services/weatherService';
+import { condenseWeatherResponse } from '@/services/weatherService';
 
 export default async function handler(
   req: NextApiRequest,
@@ -16,6 +16,13 @@ export default async function handler(
   const url = `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${latitude},${longitude}&days=3&aqi=no&alerts=no`;
   switch (req.method) {
     case 'GET':
+      try {
+        const response = await axios.get(url);
+        const condensedResponse = condenseWeatherResponse(response.data);
+        return res.status(200).json({ weather: condensedResponse });
+      } catch (error) {
+        return res.status(500).json({ message: error.message });
+      }
 
     default:
       res.setHeader('Allow', ['GET']);
--- src/services/weatherService.ts
+++ src/services/weatherService.ts
@@ -1,1 +1,23 @@
-export {};
+export function condenseWeatherResponse(response: any) {
+  const { name, region, country } = response.location;
+
+  const forecastsByDay = response.forecast.forecastday.map((forecast: any) => {
+    const { date, day } = forecast;
+    const { maxtemp_f, mintemp_f, avgtemp_f, condition } = day;
+
+    return {
+      date,
+      maxtemp_f,
+      mintemp_f,
+      avgtemp_f,
+      condition,
+    };
+  });
+
+  return {
+    name,
+    region,
+    country,
+    forecastsByDay,
+  };
+}
