0b043ba209a55ccc5564e28aa0a6f0c4baad65a4
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2023-04-04 12:25:26 -0700

    wip: Add boilerplate for summary form

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -1,3 +1,4 @@
 export { default as NewsCard } from './NewsCard/NewsCard';
 export { default as CategoryCard } from './CategoryCard/CategoryCard';
 export { default as ArticleActionButtons } from './ArticleActionButtons/ArticleActionButtons';
+export { default as SmartSummaryForm } from './SmartSummaryForm/SmartSummaryForm';
