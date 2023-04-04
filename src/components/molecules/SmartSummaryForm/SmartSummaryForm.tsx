5aa66d1dab8fa5464c782869734ac99b899b3960
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2023-04-04 16:50:32 -0700

    style: Create basic UI for smart sumary form

--- src/components/molecules/SmartSummaryForm/SmartSummaryForm.tsx
+++ src/components/molecules/SmartSummaryForm/SmartSummaryForm.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Typography, Modal } from '@mui/material';
+import { Box, Button, Typography, Modal, TextField } from '@mui/material';
 
 interface Props {
   articleTitle: string;
@@ -12,15 +12,50 @@ export default function SmartSummaryForm({
   onClose,
 }: Props) {
   return (
-    <Modal
-      open={true}
-      onClose={onClose}
-      aria-labelledby="modal-modal-title"
-      aria-describedby="modal-modal-description"
-    >
-      <Box sx={{ style }}>
-        <Typography>{articleTitle}</Typography>
-        <Typography>{articleURL}</Typography>
+    <Modal open={true} onClose={onClose}>
+      <Box sx={style}>
+        <Box>
+          <Typography variant="h6" component="h2" sx={{ textAlign: 'center' }}>
+            Smart Summary
+          </Typography>
+          <Typography color="secondary.main" textAlign="center" gutterBottom>
+            To create a smart summary, copy and paste the article content from
+            the link below.
+          </Typography>
+          <Typography textAlign="center">
+            <a
+              style={{ color: 'inherit' }}
+              href={articleURL}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {articleTitle}
+            </a>
+          </Typography>
+        </Box>
+
+        <Box
+          component="form"
+          sx={{ display: 'flex', flexDirection: 'column', gap: 1, pt: 2 }}
+        >
+          <TextField
+            id="outlined-multiline-static"
+            label="Article Content"
+            multiline
+            rows={4}
+            variant="outlined"
+            sx={{ width: '100%' }}
+            inputProps={{
+              minLength: 100,
+              maxLength: 1000,
+            }}
+            required
+          />
+
+          <Button variant="outlined" type="submit">
+            Generate Summary
+          </Button>
+        </Box>
       </Box>
     </Modal>
   );
@@ -31,9 +66,11 @@ const style = {
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: 400,
+  width: 'clamp(300px, 90vw, 500px)',
   bgcolor: 'background.paper',
-  border: '2px solid #000',
   boxShadow: 24,
-  p: 4,
+  p: 2,
+  borderRadius: 5,
+  border: 1,
+  borderColor: 'secondary.main',
 };
