f684f2f36db788b28bbf890c6c6cb54b6810537b
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2023-04-04 11:49:12 -0700

    wip: Create a function to query open ai's chatpgt model

--- src/pages/api/smart-summary.ts
+++ src/pages/api/smart-summary.ts
@@ -1,5 +1,9 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import axios from 'axios';
+import {
+  OpenAIApi,
+  Configuration,
+  ChatCompletionRequestMessageRoleEnum,
+} from 'openai';
 
 export default async function handler(
   req: NextApiRequest,
@@ -18,4 +22,28 @@ export default async function handler(
       res.setHeader('Allow', ['GET']);
       res.status(405).end(`Method ${req.method} Not Allowed`);
   }
+}
+
+async function getSummary(content: string) {
+  const config = new Configuration({
+    apiKey: process.env.OPEN_AI_KEY,
+  });
+  const openai = new OpenAIApi(config);
+
+  const prompt = `Please provide a short summary of the following article:\n\n${content}`;
+  const messages = [
+    {
+      role: ChatCompletionRequestMessageRoleEnum.System,
+      content: 'You are ChatGPT, an AI language model.',
+    },
+    { role: ChatCompletionRequestMessageRoleEnum.User, content: prompt },
+  ];
+
+  const response = await openai.createChatCompletion({
+    model: 'gpt-3.5-turbo',
+    messages,
+    max_tokens: 50,
+  });
+
+  return response.data;
 }
