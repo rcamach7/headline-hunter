fb935c4c17dc95d56b0a9b9f7cc6c90e57b83fe1
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2023-04-30 20:47:40 -0700

    ci: Configure testing routes/pages

--- next.config.js
+++ next.config.js
@@ -5,6 +5,24 @@ const nextConfig = {
   images: {
     domains: ["lh3.googleusercontent.com", "https://lh3.googleusercontent.com", "localhost", "cdn.weatherapi.com"],
   },
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    if (process.env.NEXT_PUBLIC_IS_TEST_ENV === "true") {
+      return defaultPathMap;
+    }
+
+    const productionPathMap = { ...defaultPathMap };
+
+    Object.keys(defaultPathMap).forEach((path) => {
+      if (path.includes("/test/")) {
+        delete productionPathMap[path];
+      }
+    });
+
+    return productionPathMap;
+  },
 };
 
 module.exports = nextConfig;
