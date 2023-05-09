c990f488c71ddd0ac8a35eb45874ddb7c4f65136
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2023-05-08 17:24:38 -0700

    chore: Increase requests limit

--- src/hooks/useRateLimiter.ts
+++ src/hooks/useRateLimiter.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 
-const MAX_REQUESTS = 2;
+const MAX_REQUESTS = 10;
 const TIME_WINDOW = 60 * 60 * 1000; // 1 hour
 
 export function useRateLimiter() {
