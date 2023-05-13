2a4bd43a4eb8c49303e8393343e7cf1ede0a7a3a
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2023-05-13 13:15:06 -0700

    test: Remove test configurations and directories

--- next.config.js
+++ next.config.js
@@ -4,25 +4,7 @@ const nextConfig = {
   swcMinify: true,
   images: {
     domains: ["lh3.googleusercontent.com", "https://lh3.googleusercontent.com", "localhost", "cdn.weatherapi.com"],
-  },
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    if (process.env.NEXT_PUBLIC_IS_TEST_ENV === "true") {
-      return defaultPathMap;
-    }
-
-    const productionPathMap = { ...defaultPathMap };
-
-    Object.keys(defaultPathMap).forEach((path) => {
-      if (path.includes("/test/")) {
-        delete productionPathMap[path];
-      }
-    });
-
-    return productionPathMap;
-  },
+  }
 };
 
 module.exports = nextConfig;
--- src/pages/test/index.tsx
+++
@@ -1,7 +0,0 @@
-import { useEffect } from 'react';
-const { Readability } = require('@mozilla/readability');
-import axios from 'axios';
-
-export default function Test() {
-  return <>Test</>;
-}
