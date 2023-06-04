b6c5fd2ff85b3f7d161806a6098752695de51d37
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2023-06-04 13:49:16 -0700

    test: Remove populate function no longer needed

--- src/lib/test.ts
+++
@@ -1,6 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-
-export async function testHandler(req: NextApiRequest, res: NextApiResponse) {
-  console.log('Test API Endpoint Hit');
-  res.send('Test API Endpoint Hit');
-}
