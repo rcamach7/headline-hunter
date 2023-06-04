6d843883ba3b8da854adade425f0515dccda1fda
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2023-06-04 13:41:38 -0700

    refactor: Remove data file and fix imports

--- src/services/categoryService.ts
+++ src/services/categoryService.ts
@@ -1,6 +1,5 @@
 import prisma from '@/lib/prisma';
 
-import { popularCategories } from '@/lib/data';
 import { getUserByEmail } from './userService';
 
 export const getCategoryById = async (id: string) => {
@@ -86,7 +85,7 @@ export async function getInitialCategories(session: any) {
     }
   } else {
     return await getCategoriesByIds(
-      popularCategories.map((category) => category.id)
+      defaultCategories.map((category) => category.id)
     );
   }
 }
@@ -103,3 +102,31 @@ export async function getAdditionalCategories(current: string[]) {
     take: 2,
   });
 }
+
+export const defaultCategories = [
+  {
+    id: '77d0b6a2-4685-4a5e-9d60-87e05fa5356e',
+    type: 'business',
+    lastUpdated: null,
+  },
+  {
+    id: '2c6a96f2-25cc-4afd-b78a-eeeb54e58847',
+    type: 'entertainment',
+    lastUpdated: null,
+  },
+  {
+    id: '928a68a2-7387-4305-b8a8-870ba0929df6',
+    type: 'sports',
+    lastUpdated: null,
+  },
+  {
+    id: '44d25b34-6778-41cf-a09b-75f7fba28fec',
+    type: 'technology',
+    lastUpdated: null,
+  },
+  {
+    id: 'b1dfe2c4-14e0-4a14-8ff4-ed8a7df49601',
+    type: 'politics',
+    lastUpdated: null,
+  },
+];
