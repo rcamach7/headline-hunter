90fdf7b5a230013e294b2cd36d56ccbc46ef829a
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2023-06-20 13:15:37 -0700

    fix: Add home option back when not in homepage

--- src/components/organisms/AppBar/UserMenuItems.tsx
+++ src/components/organisms/AppBar/UserMenuItems.tsx
@@ -1,5 +1,8 @@
+import { useState } from 'react';
 import { Typography, MenuItem } from '@mui/material';
 import { signIn, signOut } from 'next-auth/react';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 import { User } from '@/lib/types';
 import ExternalArticleSummary from './ExternalArticleSummary';
@@ -10,8 +13,18 @@ interface Props {
 }
 
 export default function UserMenuItems({ user }: Props) {
+  const router = useRouter();
   return (
     <>
+      {router.pathname !== '/' && (
+        <MenuItem>
+          <Typography textAlign="center">
+            <Link href="/">
+              <a style={{ textDecoration: 'none', color: 'inherit' }}>Home</a>
+            </Link>
+          </Typography>
+        </MenuItem>
+      )}
       <ExternalArticleSummary />
       <Settings user={user} />
       {user ? (
